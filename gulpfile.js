8782b56fdae77b839ad96145f99de79f397a4492
James Allen <code@james-allen.co.uk>
2014-10-02 22:05:19 +0000

    Update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,55 @@
 var gulp = require('gulp'),
-	jshint = require('gulp-jshint'),
-	watch = require('gulp-watch'),
-	// changed = require('gulp-changed'),
-	mocha = require('gulp-mocha'),
-	istanbul = require('gulp-istanbul'),
-	colors = require('colors'),
-	coveralls = require('gulp-coveralls');
+    jshint = require('gulp-jshint'),
+    watch = require('gulp-watch'),
+    // changed = require('gulp-changed'),
+    mocha = require('gulp-mocha'),
+    istanbul = require('gulp-istanbul'),
+    colors = require('colors'),
+    coveralls = require('gulp-coveralls');
 
 var paths = {
-	'src':['./sitemark.js', './lib/**/*.js','./routes/**/*.js'],
-	'tests':['./test/**/*.js']
+    'src': ['./sitemark.js', './lib/**/*.js', './routes/**/*.js'],
+    'tests': ['./test/**/*.js']
 };
 
 // An error handler for the tests during gulp-watch
 // Otherwise the gulp-watch will terminate
-var handleError = function(err){
-	console.log((err.name+': '+err.plugin+' - '+err.message).red);
-	// propogate
-	return;
+var handleError = function(err) {
+    console.log((err.name + ': ' + err.plugin + ' - ' + err.message).red);
+    // propogate
+    return;
 };
 
 // gulp lint
-gulp.task('lint', function(){
-	gulp.src(paths.src)
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'));
+gulp.task('lint', function() {
+    gulp.src(paths.src)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
 
 });
 
 // gulp for running the mocha tests with default dot reporter
-gulp.task('test', function(cb){
+gulp.task('test', function(cb) {
     gulp.src(paths.src)
-	    .pipe(istanbul())
-		.on('finish', function(){
-		gulp.src(paths.tests)
-			.pipe(mocha({
-	 	        require: 'should',
-	            reporter: 'spec'
-	        }))
-			.pipe(istanbul.writeReports())
-			.on('end', function(){
-				if (process.env.COVERALLS_REPO_TOKEN) {
-					gulp.src('coverage/**/lcov.info')
-	  					.pipe(coveralls())
-						.on('end', cb);
-					}
-				})
-			.on('error', handleError);
-		});
+        .pipe(istanbul())
+        .on('finish', function() {
+            gulp.src(paths.tests)
+                .pipe(mocha({
+                    require: 'should',
+                    reporter: 'spec'
+                }))
+				.pipe(istanbul.writeReports({
+					reporters: ['lcov', 'json', 'text', 'text-summary', 'html', 'clover']
+				}))
+                .on('end', function() {
+                    if (process.env.COVERALLS_REPO_TOKEN) {
+                        gulp.src('coverage/**/lcov.info')
+                            .pipe(coveralls())
+                            .on('end', cb);
+                    }
+                })
+                .on('error', handleError);
+        });
 });
 
 
@@ -55,8 +57,8 @@ gulp.task('test', function(cb){
  * auto/watch gulp tasks that will trigger the tests on
  * file changes
  */
-gulp.task('watch', function(){
-	gulp.watch(paths.src.concat(paths.tests), ['lint','test']);
+gulp.task('watch', function() {
+    gulp.watch(paths.src.concat(paths.tests), ['lint', 'test']);
 });
 
 gulp.task('default', ['lint', 'test']);
